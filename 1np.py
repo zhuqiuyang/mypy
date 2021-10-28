68a980dd3df8d4d7a2f5b9d874efebcbe3979ef6
Ace <zqyhit@qq.com>
2021-10-28 02:59:14 +0000

    update numpy

---
+++ 1np.py
@@ -0,0 +1,20 @@
+import numpy as np
+a = np.arange(15).reshape(3, 5)
+print(a)
+print(a.shape)
+
+ 
+print(a.ndim)
+
+print(a.dtype.name)
+
+print(a.itemsize)
+
+print(a.size)
+
+type(a)
+
+b = np.array([6, 7, 8])
+b
+
+type(b)
